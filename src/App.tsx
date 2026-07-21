743a1b11c47769e5f01ea1fc61c959e08c2c110b
Samantha Goldstein <samantha.goldstein31@gmail.com>
2026-07-21 13:08:16 -0400

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from '/after_hours.png'
-import viteLogo from '/campcamplogo.png'
-import heroImg from '/waves.jpg'
+import heroImg from '/assets/shader1.jpg'
+import campCampLogo from '/campcamplogo.png'
 // import './App.css'
 
 function App() {
@@ -9,112 +8,35 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <div>
+        <div className="hero relative">
+          <img src={heroImg} className="circle-mask " alt="A gooey groovy swirl" />
+          <span className="border hero-border rounded-[50%] top-0 right-0 md:top-1 md:right-1"></span>
+          <img src={campCampLogo} className="camp-camp-logo" alt="CAMP CAMP" />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className="border">
+          <h1 className=""> Camp Camp </h1>
+          <p> a celebration of art, music, and camp</p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        <div className="border">
+          <p>September 10 - 13</p>
+          <p>Thurs - Sun</p>
+          <br />
+          <p>Camp Gregory</p>
+          <p>Aurora, NY</p>
+        </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <div className="border">
+          Schedule
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div className="border">
+          Map
         </div>
-      </section>
+        <div className="border">
+          FAQs
+        </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
     </>
   )
 }

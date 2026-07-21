30275afcd50f8ff81e318ab41364a827fb451a85
Samantha Goldstein <samantha.goldstein31@gmail.com>
2026-07-21 14:01:10 -0400

    setting page structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <div className="hero relative">
-          <img src={heroImg} className="circle-mask " alt="A gooey groovy swirl" />
-          <span className="border hero-border rounded-[50%] top-0 right-0 md:top-1 md:right-1"></span>
-          <img src={campCampLogo} className="camp-camp-logo" alt="CAMP CAMP" />
+      <div className="flex flex-wrap justify-center">
+        <div>
+          <div className="hero relative">
+            <img src={heroImg} className="circle-mask " alt="A gooey groovy swirl" />
+            <span className="border hero-border rounded-[50%] top-0 left-0 md:top-1 md:left-1"></span>
+            <img src={campCampLogo} className="camp-camp-logo" alt="CAMP CAMP" />
+          </div>
+
         </div>
-        <div className="border">
+
+        <div className="border max-w-md">
           <h1 className=""> Camp Camp </h1>
-          <p> a celebration of art, music, and camp</p>
+          <p className="text-6xl"> a celebration of art, music, and camp</p>
         </div>
         <div className="border">
           <p>September 10 - 13</p>
